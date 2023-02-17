0fe6c4f87d335cf0812f70b4cf0162e47c1dedb6
MustafaAldolimy <100897710+DevM0714@users.noreply.github.com>
2023-02-18 01:36:48 +0300

    add functions and fix logic in main

--- src/main/kotlin/interactor/GetCityThatMatchManagerExpectationsInteractor.kt
+++ src/main/kotlin/interactor/GetCityThatMatchManagerExpectationsInteractor.kt
@@ -5,11 +5,13 @@ import model.CityEntity
 class GetCityThatMatchManagerExpectationsInteractor(
 private val dataSource: CostOfLivingDataSource
 ) {
-    fun execute(): List<CityEntity> {
+    fun execute(country: String): List<String> {
         return dataSource
             .getAllCitiesData()
             .filter(::excludeNullMealsPricesAndLowQualityData)
-
+            .filter {it.country == filteringBasedOnCountry(country)}
+            .filter(::takeIfAverageMealPriseIsBetweenMinAndMax)
+            .map { it.cityName }
     }
     private fun excludeNullMealsPricesAndLowQualityData(city: CityEntity): Boolean {
         return city.mealsPrices.mealInexpensiveRestaurant != null &&
@@ -17,4 +19,35 @@ private val dataSource: CostOfLivingDataSource
                city.mealsPrices.mealFor2PeopleMidRangeRestaurant != null &&
                city.dataQuality
     }
+
+    private fun minMealPrice(city: CityEntity): Float = minOf(
+            city.mealsPrices.mealAtMcDonaldSOrEquivalent!!,
+            city.mealsPrices.mealFor2PeopleMidRangeRestaurant!!.div(2),
+            city.mealsPrices.mealInexpensiveRestaurant!!
+        )
+
+    private fun maxMealPrice(city: CityEntity): Float = maxOf(
+        city.mealsPrices.mealAtMcDonaldSOrEquivalent!!,
+        city.mealsPrices.mealFor2PeopleMidRangeRestaurant!!.div(2),
+        city.mealsPrices.mealInexpensiveRestaurant!!
+    )
+
+    private fun filteringBasedOnCountry(country: String) =
+        country.takeIf { it in listOf("United States", "Canada", "Mexico") }
+
+
+    private fun getAverageMealPrise(city: CityEntity): Float =
+        (minMealPrice(city) + maxMealPrice(city)).div(2)
+
+    private fun takeIfAverageMealPriseIsBetweenMinAndMax(city: CityEntity) =
+        getAverageMealPrise(city) in minMealPrice(city)..maxMealPrice(city)
+
+//        getAverageMealPrise(city) in setOf(
+//            city.mealsPrices.mealAtMcDonaldSOrEquivalent,
+//            city.mealsPrices.mealFor2PeopleMidRangeRestaurant?.div(2),
+//            city.mealsPrices.mealInexpensiveRestaurant
+//        )
+    //
+
+
 }
