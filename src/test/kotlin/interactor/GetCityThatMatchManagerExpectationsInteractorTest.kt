41e9e9c49c936fe9ac114ca4f40455e2683e3a11
MustafaAldolimy <100897710+DevM0714@users.noreply.github.com>
2023-02-19 01:03:49 +0300

    Modify test file and add one test case

--- src/test/kotlin/interactor/GetCityThatMatchManagerExpectationsInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityThatMatchManagerExpectationsInteractorTest.kt
@@ -1,12 +1,29 @@
 package interactor
 
+import FakeData
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
-
+import org.junit.jupiter.api.TestInstance
+import kotlin.test.assertEquals
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GetCityThatMatchManagerExpectationsInteractorTest {
+    private lateinit var getCityThatMatchManagerExpectationsInteractor: GetCityThatMatchManagerExpectationsInteractor
+    private lateinit var fakeData: FakeData
 
+
+    @BeforeAll
+     fun setUp() {
+        fakeData = FakeData()
+        getCityThatMatchManagerExpectationsInteractor = GetCityThatMatchManagerExpectationsInteractor(fakeData)
+    }
     @Test
-    fun execute() {
+    fun should_ReturnEmptyList_when_InputIsNotUnitedStatesOrCanadaOrMexico() {
+        //give the country name
+        val countryName = "iraq"
+        //when the expected is empty list and the actual is the execute function
+        val expected: List<String?> = emptyList()
+        val actual= getCityThatMatchManagerExpectationsInteractor.execute(countryName)
+        //then
+        assertEquals(expected,actual)
     }
 }
