1057e48af926a95807ce95cbfa5e40ce9ade758a
MustafaAldolimy <100897710+DevM0714@users.noreply.github.com>
2023-02-19 00:14:16 +0300

    fix the logic

--- src/main/kotlin/interactor/GetCityThatMatchManagerExpectationsInteractor.kt
+++ src/main/kotlin/interactor/GetCityThatMatchManagerExpectationsInteractor.kt
@@ -4,14 +4,18 @@ import model.CityEntity
 
 class GetCityThatMatchManagerExpectationsInteractor(
 private val dataSource: CostOfLivingDataSource
-) {
+)
+{
     fun execute(country: String): List<String> {
         return dataSource
             .getAllCitiesData()
-            .filter(::excludeNullMealsPricesAndLowQualityData)
-            .filter {it.country == filteringBasedOnCountry(country)}
-            .filter(::takeIfAverageMealPriseIsBetweenMinAndMax)
-            .map { it.cityName }
+            .asSequence()
+            .filter{excludeNullMealsPricesAndLowQualityData(it) && it.country == filteringBasedOnCountry(country)}
+            .filter(::takeIfAverageMealPriceIsBetweenMinAndMax)
+            .sortedBy { getAverageMealPrice(it) }
+            .take(1)
+            .map {it.cityName}
+            .toList()
     }
     private fun excludeNullMealsPricesAndLowQualityData(city: CityEntity): Boolean {
         return city.mealsPrices.mealInexpensiveRestaurant != null &&
@@ -36,18 +40,10 @@ private val dataSource: CostOfLivingDataSource
         country.takeIf { it in listOf("United States", "Canada", "Mexico") }
 
 
-    private fun getAverageMealPrise(city: CityEntity): Float =
+    private fun getAverageMealPrice(city: CityEntity): Float =
         (minMealPrice(city) + maxMealPrice(city)).div(2)
 
-    private fun takeIfAverageMealPriseIsBetweenMinAndMax(city: CityEntity) =
-        getAverageMealPrise(city) in minMealPrice(city)..maxMealPrice(city)
-
-//        getAverageMealPrise(city) in setOf(
-//            city.mealsPrices.mealAtMcDonaldSOrEquivalent,
-//            city.mealsPrices.mealFor2PeopleMidRangeRestaurant?.div(2),
-//            city.mealsPrices.mealInexpensiveRestaurant
-//        )
-    //
-
+    private fun takeIfAverageMealPriceIsBetweenMinAndMax(city: CityEntity) : Boolean =
+       getAverageMealPrice(city) in minMealPrice(city)..maxMealPrice(city)
 
 }
